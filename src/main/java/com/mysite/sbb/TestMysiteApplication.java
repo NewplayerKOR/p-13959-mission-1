0d47c5a8e3ec195f9f19ac8b14cbf2257559c077
NewplayerKOR <149351553+NewplayerKOR@users.noreply.github.com>
2025-06-24 21:17:26 +0900

    chapter2_commit_done

---
+++ src/main/java/com/mysite/sbb/TestMysiteApplication.java
@@ -0,0 +1,12 @@
+package com.mysite.sbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMysiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMysiteApplication.class, args);
+    }
+}
